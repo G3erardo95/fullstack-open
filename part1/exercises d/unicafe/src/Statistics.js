2460f94f0b6824d4bf52284048a2594cb423f488
Gerardo <gerardo.dbl@gmail.com>
2022-04-05 19:52:08 +0200

    from 1.7 to 1.11

---
+++ part1/exercises d/unicafe/src/Statistics.js
@@ -0,0 +1,18 @@
+import { StatisticLine } from "./StatisticLine";
+
+export const Statistics = ({ good, neutral, bad, total }) => {
+  if (total != 0) {
+    return (
+      <>
+        <StatisticLine text={"Good"} value={good} />
+        <StatisticLine text={"Neutral"} value={neutral} />
+        <StatisticLine text={"Bad"} value={bad} />
+        <StatisticLine text={"Total"} value={total} />
+        <StatisticLine text={"Average"} value={(good - bad) / total} />
+        <StatisticLine text={"Positive"} value={(good / total) * 100}/>
+      </>
+    );
+  } else {
+    return <h2> No feedback given</h2>;
+  }
+};
